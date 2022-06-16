e3632c87700df9e26177e99aeea01b582ef31193
Ennio-M <marrese.ennio@gmail.com>
2022-06-17 00:32:44 +0200

    esercizio completo

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('/home');
 });
 
-Route::resource('comics', 'ComicController');
+Route::resource('/', 'ComicController');
